1fe29515285ecb82bcad42e5014d53ff5d328989
seba9rojas <142066213+seba9rojas@users.noreply.github.com>
2024-04-28 16:22:28 -0400

    Create Entrada.java

---
+++ Entrada.java
@@ -0,0 +1,21 @@
+public class Entrada {
+    private String id;
+    private boolean disponible;
+
+    // Getters y Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+}
